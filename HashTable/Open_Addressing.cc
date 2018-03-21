30970a41a7ef6a0f234a275f5a4c5f15fe600c17
Mingkai Ma <mamingkai824@gmail.com>
2018-03-21 16:45:33 +1100

    open addressing

--- HashTable/Open_Addressing.cc
+++ HashTable/Open_Addressing.cc
@@ -19,3 +19,18 @@ void Hash_Insert(T, k)
 
     cout << "hash table overflow"
 }
+
+void Hash_Search(T, k)
+{
+    i = 0;
+    do
+    {
+        j = h(k, i);
+        if T[j] == k
+            return j;
+
+        i += 1;
+    }until(T[j] == NULL || i == m)
+
+    return NULL;
+}
