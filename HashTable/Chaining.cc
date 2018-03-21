ddb8e95209a167d89fc3395d583ddff139d40d18
Mingkai Ma <mamingkai824@gmail.com>
2018-03-21 16:21:36 +1100

    chaining

---
+++ HashTable/Chaining.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// use chaining to deal with the collision
+
+void Chained_Hash_Insert(T, x)
+{
+    insert x at the head of list T[h(x.key)];
+}
+
+void Chained_Hash_Search(T, k)
+{
+    search for an element with key k in list T[h(k)];
+}
+
+void Chained_Hash_Delete(T, x)
+{
+    delete x from the list T[h(x.key)];
+}
