d3ea3c5dd501482cae3554a3505891d689b2826d
Mingkai Ma <mamingkai824@gmail.com>
2018-03-26 16:20:06 +1100

    BST search

--- BinarySearchTree/BST_Search.cc
+++ BinarySearchTree/BST_Search.cc
@@ -1,4 +1,6 @@
 
+
+//  recursion
 BST* Search(BST* root, int value)
 {
     if(root == NULL || k == root->val)
@@ -9,4 +11,22 @@ BST* Search(BST* root, int value)
         return Search(root->left, value);
 
     return Search(root->right, value);
+}
+
+
+
+//  while loop to replace recursion
+
+BST* Iterative_Search(BST* root, int value)
+{
+    while(x != NULL && value != root->val)
+    {
+        if(root->val > value)
+            root = root->left;
+
+        else
+            root = root->right;
+
+    }
+    return root;
 }
