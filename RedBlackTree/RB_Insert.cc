a2a919a03d6844f5d2aa68bc002bb1ae4c989450
Mingkai Ma <mamingkai824@gmail.com>
2018-03-27 14:57:20 +1100

    RB tree insert

--- RedBlackTree/RB_Insert.cc
+++ RedBlackTree/RB_Insert.cc
@@ -29,3 +29,38 @@ void RB_Insert(BST* T, BST* z)
     RB_Insert_Fixup(T, z);
 }
 
+void RB_Insert_Fixup(BST* T, BST* z)
+{
+    while(z->parent->color == RED)
+    {
+
+        if(z->parent == z->parent->parent->left)
+        {
+
+            BST* y = z->parent->parent->right;
+            if(y->color == RED)
+            {
+                z->parent->color = BLACK;
+                y->color = BLACK;
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            }
+
+            else if(z == z->parent->right)
+            {
+
+                z = z->parent;
+                leftRotate(T, z);
+            }
+
+            z->parent->color = BLACK;
+            z->parent->parent->color = RED;
+            rightRotate(T, z->parent->parent);
+
+
+        }
+        else
+            ;//same as then clause with "right" and "left" exchanged
+    }
+    T->color = BLACK;
+}
