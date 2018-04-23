c8a73b80c52091b5855c34c21bfd90d920040362
Mingkai Ma <mamingkai824@gmail.com>
2018-04-23 17:31:23 +1000

    Integer_Knapsack_Problem_Duplicated_Allowed

---
+++ Dynamic-Programming/Integer_Knapsack_Problem_Duplicated_Allowed.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int DP(int value[], int weight[], int n, int C, vector<int>& backtrack)
+{
+    int* M = new int[C + 1];
+    M[0] = 0;
+    int i, j, tmp, pos;
+    for(i = 1; i <= C; i++)
+    {
+        M[i] = M[i - 1];
+        pos = i - 1;
+        for(j = 0; j < n; j++)
+        {
+            if(i >= weight[j])
+                tmp = M[i - weight[j]] + value[j];
+            if(tmp > M[i])
+            {
+                M[i] = temp;
+                pos = i - weight[j];
+            }
+
+        }
+
+        backtrack.push_back(pos);
+
+    }
+    int res = M[C];
+    delete[] M;
+    return res;
+}
