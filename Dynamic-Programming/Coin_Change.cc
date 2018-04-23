d25f5c647c1fa9c1d28433edc7301308ce89344a
Mingkai Ma <mamingkai824@gmail.com>
2018-04-23 16:25:40 +1000

    coin change

--- Dynamic-Programming/Coin_Change.cc
+++ Dynamic-Programming/Coin_Change.cc
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+// Recursion
 int count(int S[], int m, int n)
 {
     if(n == 0)
@@ -17,9 +19,33 @@ int count(int S[], int m, int n)
     return count(S, m - 1, n) + count(S, m, n - S[m - 1]);
 }
 
+// Dynamic Programming
+
+int DP_count(int S[], int m, int n)
+{
+    int i, j, x, y;
+
+    int table[n + 1][m];
+
+    for(i = 0; i < m; i++)
+        table[0][i] = 1;
+
+    for(i = 1; i < n + 1; i++)
+    {
+        for(j = 0; j < m; j++)
+        {
+            x = (i-S[j] >= 0)? table[i - S[j]][j]: 0;
+            y = (j >= 1)? table[i][j-1]: 0;
+            table[i][j] = x + y;
+        }
+    }
+
+    return table[n][m - 1];
+}
+
 int main()
 {
-    int i, j;
+
     int arr[] = {1, 2, 3};
     int m = sizeof(arr) / sizeof(arr[0]);
     cout << count(arr, m, 4) << endl;
