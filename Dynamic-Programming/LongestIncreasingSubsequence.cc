73a9bc6e6e45c5d6cf7fb42989e137f41e06b968
Mingkai Ma <mamingkai824@gmail.com>
2018-04-23 15:50:19 +1000

    LongestIncreasingSubsequence

--- Dynamic-Programming/LongestIncreasingSubsequence.cc
+++ Dynamic-Programming/LongestIncreasingSubsequence.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // recursion
@@ -107,10 +108,13 @@ int LIS(vector<int>& v)
 
 
 
-
 int main()
 {
-    int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cout << DP_LIS(arr, n) << endl;
+//    int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//    cout << DP_LIS(arr, n) << endl;
+
+
+    vector<int> arr = { 10, 22, 9, 33, 21, 50, 41, 60 };
+    cout << LIS(arr) << endl;
 }
