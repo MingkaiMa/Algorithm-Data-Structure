b96d1ad88c6f7d952bd2ca2f7a9b4a93fdf6b9d7
Mingkai Ma <mamingkai824@gmail.com>
2018-04-30 16:28:19 +1000

    string matching

---
+++ String-Matching/naive_string_matcher.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void naive_string_matcher(string T, string P)
+{
+    int n = T.size();
+    int m = P.size();
+
+    for(int s = 0; s <= n - m; s++)
+    {
+        string newString(T.begin() + s, T.begin() + s + m);
+        if(newString == P)
+            cout << "Pattern occurs with shift " << s << endl;
+    }
+}
+
+int main()
+{
+    string T = "abcde";
+    string P = "cde";
+
+    naive_string_matcher(T, P);
+}
